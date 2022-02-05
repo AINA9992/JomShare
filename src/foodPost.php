4120d913c657ba0090028930379615fd1c27d242
aisyahsohimi <77560946+aisyahsohimi@users.noreply.github.com>
2022-02-06 01:46:04 +0800

    Update foodPost.php

--- src/foodPost.php
+++ src/foodPost.php
@@ -1,32 +1,57 @@
-<?php
-$connection = mysqli_connect("localhost","root",null,"foodPost");
-
-
-//check connection
-    if($sql_connection){
-        echo "SAVE YOUR NOTE HERE";
-    }
-
-    echo "<br><br>";
-
-    if(isset($_POST['save'])) {
-        $name = $_POST['NAME'];
-        $date=$_POST['DATE'];
-        $email = $_POST['EMAIL'];
-        $title = $_POST['TITLE'];
-        $notes = $_POST['NOTES'];
-
-        $sql = "INSERT INTO users (ID, NAME, DATE, EMAIL,TITLE,NOTES) VALUES(null,'$name','$date','$email','$title','$notes')";
-        if (mysqli_query($sql_connection, $sql)) {
-            echo "New data has been update successfully.";
-        } else {
-            echo "Error " . $sql . "" . mysqli_error($sql_connection);
+<!DOCTYPE html>
+<html>
+  
+<head>
+    <title>Insert Page page</title>
+</head>
+  
+<body>
+    <center>
+        <?php
+  
+        // servername => localhost
+        // username => root
+        // password => empty
+        // database name => staff
+        $conn = mysqli_connect("localhost","root",null,"foodpost");
+          
+        // Check connection
+        if($conn === false){
+            die("ERROR: Could not connect. " 
+                . mysqli_connect_error());
         }
-    }
-    $all="SELECT NAME, DATE, EMAIL, TITLE, NOTES FROM users";
-    $result=mysqli_query($sql_connection, $all);
-    $data=mysqli_fetch_all($result,MYSQLI_ASSOC) ;
-    mysqli_close($sql_connection);
+          
+        // Taking all 5 values from the form data(input)
+        $username =  $_REQUEST['username'];
+        $picture = $_REQUEST['picture'];
+        $contact_detail =  $_REQUEST['contact_detail'];
+        $pickup_location = $_REQUEST['pickup_location'];
+        $expired_date = $_REQUEST['expired_date'];
+        $food_description = $_REQUEST['food_description'];
 
-    ?>    
+          
+        // Performing insert query execution
+        // here our table name is college
+        $sql = "INSERT INTO foodpost  VALUES ('$username', 
+            '$picture','$contact_detail','$pickup_location','$expired_date','$food_description')";
+          
+        if(mysqli_query($conn, $sql)){
+            echo "<h3>data stored in a database successfully." 
+                . " Please browse your localhost php my admin" 
+                . " to view the updated data</h3>"; 
+  
+            echo nl2br("\n$username\n $picture\n "
+                . "$contact_detail\n $pickup_location\n $expired_date\n $food_description");
+        } else{
+            echo "ERROR: Hush! Sorry $sql. " 
+                . mysqli_error($conn);
+        }
+          
+        // Close connection
+        mysqli_close($conn);
+        ?>
+    </center>
+</body>
+  
+</html>
  
